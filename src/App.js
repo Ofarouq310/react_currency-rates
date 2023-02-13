096368636e284bdd4d400f44466863480f78f870
Omar Farouq <ofarouq310@gmail.com>
2023-02-13 17:10:59 +0200

    update main js files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Details from './components/Details';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { fetchCurrency } from './redux/currency/currency';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrency());
+  }, [dispatch]);
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </div>
+  );
 }
