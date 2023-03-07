c3ebd816f08d87444a5fa917662a185e28bff9ef
Omar Farouq <ofarouq310@gmail.com>
2023-03-07 15:36:15 +0200

    Update main pages component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,35 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
 export default function Home() {
-  const currency = useSelector((state) => state.currency.allData.usd);
-  const date = useSelector((state) => state.currency.allData.date);
-  const currencyList = Array.from(Object.entries(currency || {})).map((item) => (
-    <div key={item[0]}>
-      <p>
-        USD:
-        {' '}
-        {item[1]}
-        {' '}
-        {item[0]}
-      </p>
-    </div>
-  ));
   return (
-    <div>
-      <h2>
-        Date:
-        {' '}
-        {date}
-      </h2>
-      {currencyList}
+    <div className="homepage--container">
+      <div className="bkg-anim" />
+      <div className="bkg-anim" />
+      <div className="header-wrapper">
+        <h1 id="homepage-header">
+          TRACK
+          <span className="grey"> CURRENCIES </span>
+          RATES
+          <span className="grey"> AROUND </span>
+          THE WORLD
+          <span className="grey"> DAY </span>
+          BY
+          <span className="grey"> DAY </span>
+        </h1>
+        <div type="button" id="homepage-btn">
+          <a href="/currencies_list">
+            <div className="btn-left--arrow">
+              <i className="fa-solid fa-chevron-right" />
+            </div>
+            <p>Currencies rates</p>
+            <div className="btn-right--arrows">
+              <i className="fa-solid fa-angle-right" />
+              <i className="fa-solid fa-angle-right" />
+              <i className="fa-solid fa-angle-right" />
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
